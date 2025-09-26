7bb684ef803a64fee899ecfc9f8b1b578f3e2942
Subin7942 <209975811+Subin7942@users.noreply.github.com>
2025-09-26 11:43:47 +0900

    냥

--- src/w3/step6/main.js
+++ src/w3/step6/main.js
@@ -1,36 +1,35 @@
-let x, y;
-let w, h;
-let angle;
-let colour;
-const area = 20;
 const palette = ['#FEF3E2', '#FAB12F', '#FA812F', '#DD0303'];
+
+let ps = [];
 
 function setup() {
   createCanvas(800, 600);
-}
 
-function drawRect() {
-  fill(colour);
-  noStroke();
-  push();
-  translate(x, y);
-  rotate(radians(angle));
-  rect(0.5 * w, 0.5 * h, w, h);
-  pop();
+  for (let n = 0; n < 1500; n++) {
+    ps.push(new Particle(random(width), random(height), 20));
+  }
 }
 
 function draw() {
   background(127);
+  //   for(let idx = 0; idx < ps.length; idx++){
+  //   ps[idx].drawRect();
+  // }
 
-  randomSeed(10);
-  for (let n = 0; n < 1000; n++) {
-    x = random(width);
-    y = random(height);
-    w = random(4, area);
-    h = area / w;
-    angle = random(360);
-    let paletteIdx = floor(random(palette.length));
-    colour = palette[paletteIdx];
-    drawRect();
-  }
+  // for(let idx = 0; idx < ps.length; idx++){
+  //   const aParticle = ps[idx];
+  //   aParticle.drawRect();
+  // }
+
+  // for (const aParticle of ps) {
+  //   aParticle.drawRect();
+  // }
+
+  // ps.forEach((aParticle, idx) => {
+
+  // });
+
+  ps.forEach((aParticle, idx) => {
+    aParticle.drawRect();
+  });
 }
