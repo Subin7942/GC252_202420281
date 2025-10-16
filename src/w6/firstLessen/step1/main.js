34e6983f0b3aa19a8f4e484d535e731f8a8776fc
Subin7942 <209975811+Subin7942@users.noreply.github.com>
2025-10-16 17:02:16 +0900

    w6 뱀 만들기

--- src/w6/firstLessen/step1/main.js
+++ src/w6/firstLessen/step1/main.js
@@ -8,11 +8,12 @@ function setup() {
 
   for (let n = 0; n < pointsNum; n++) {
     const x = width / 2;
-    const y = height / 4 + 30 * n;
+    const y = height / 4 + 50 * n;
     const hue = map(n, 0, pointsNum - 1, 0, 240);
     const colour = color(hue, 100, 100);
     const options = {
       colour: colour,
+      distConstraint: 50,
     };
     const newPoint = new Point(x, y, options);
     points.push(newPoint);
